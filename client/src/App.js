d9b547de5d74c0685b0d9c6557c507a139a590fa
Matthew Castillo <blacksheep3y@gmail.com>
2020-04-19 19:14:42 -0400

    Rendered player data for at least the first data set

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,28 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+    .then(res => res.json())
+    .then(player => {
+      this.setState({ ...this.state, wwc: player });
+      console.log('WWC DATA: ', this.state.wwc);
+    })
+    .catch(err => {
+      console.log("Err: ", err);
+    });
+  }
 
   render() {
     return (
       <div className="App-container">
         <h1 className="App-title">Welcome World Cup Players!</h1>
-        <WWC />
+        <WWC 
+        propName={this.state.wwc.map(item => (item.name))}
+        propCountry={this.state.wwc.map(item => (item.country))}
+        propSearches={this.state.wwc.map(item => (item.searches))}
+        propId={this.state.wwc.map(item => (item.id))}
+        />
       </div>
     );
   }
